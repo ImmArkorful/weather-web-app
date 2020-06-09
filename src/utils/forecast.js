4c4b4e9037e32667c8fa02643dfcee7578c65a92
ImmArkorful <e.arkorful@gmail.com>
2020-06-09 20:34:35 +0000

    Added new weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (location,callback) =>{
             callback('Invalid location, try a different location',undefined);
         } else{
             const strings =body.weather[0].description
-            callback(undefined,`${strings.charAt(0).toUpperCase()}${strings.slice(1)} - It is currently ${body.main.temp} degrees with humidity of ${body.main.humidity}`);
+            callback(undefined,`${strings.charAt(0).toUpperCase()}${strings.slice(1)} - It is currently ${body.main.temp} degrees with humidity of ${body.main.humidity}.
+            It feels like ${body.main.feels_like}, with air pressure of ${body.main.pressure} millibars, maximum temperature of ${body.main.temp_max} and and minimum temperature of ${body.main.temp_min}`);
         }
     })
 
