aaf59f13508d7e290124005836ed342689c55c57
ImmArkorful <e.arkorful@gmail.com>
2020-07-06 22:37:38 +0000

    feat: added new button

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,62 @@
-const weatherForm=document.querySelector('form');
+const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
-const messageOne=document.querySelector('#message-1');
-const messageTwo=document.querySelector('#message-2');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+const $location = document.querySelector('#location');
 
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    const location = search.value;
-    messageOne.textContent = 'Loading...';
-    messageTwo.textContent = '';
+  const location = search.value;
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
 
-    fetch('/weather?address='+location).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            messageOne.textContent=data.error;
-            
-        }else{
-        messageOne.textContent= data.forecast;
-        messageTwo.textContent= data.location;
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.forecast;
+        messageTwo.textContent = data.location;
+      }
+    });
+  });
+});
+
+$location.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (!navigator.geolocation) {
+    return alert('Your browser does not support the geolocation');
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      messageOne.textContent = 'Loading...';
+      messageTwo.textContent = '';
+
+      fetch('/weather?address=' + longitude + ',' + latitude).then(
+        (response) => {
+          console.log(response);
+
+          response.json().then((data) => {
+            console.log(data);
+
+            if (data.error) {
+              messageOne.textContent = data.error;
+            } else {
+              messageOne.textContent = data.forecast;
+              messageTwo.textContent = data.location;
+            }
+          });
         }
-    })
-})
-})
+      );
+    },
+    (error) => {
+      console.log(error);
+    },
+    { enableHighAccuracy: true }
+  );
+});
--- src/app.js
+++ src/app.js
@@ -1,100 +1,99 @@
-const path =  require('path');
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handle bars engine and views location
-app.set('view engine','hbs');
-app.set('views',viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('',(req,res)=>{
-    res.render('index', {
-        title: 'Weather',
-        name : 'Emmanuel Arkorful'
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Emmanuel Arkorful',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Emmanuel Arkorful',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This displays the help you need for this site',
+    name: 'Emmanuel Arkorful',
+  });
+});
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'Address must be provided!',
     });
-})
+  } else {
+    geocode({ name: req.query.address }, (error, { location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(location, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
 
-app.get('/about', (req,res)=>{
-    res.render('about',{
-        title : 'About me',
-        name : 'Emmanuel Arkorful'
+        res.send({
+          location: location,
+          address: req.query.address,
+          forecast: forecastData,
+        });
+      });
     });
-})
+  }
+});
 
-app.get('/help', (req,res) =>{
-    res.render('help', {
-        title : 'Help',
-        message : 'This displays the help you need for this site',
-        name : 'Emmanuel Arkorful'
-    })
-})
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
-      return res.send({
-            error : 'Address must be provided!'
-        })
-    }else{
-        geocode(req.query.address, (error,{location} = {})=>{
-            if(error){
-                return res.send({error});
-            }
-                forecast(location,(error,forecastData)=>{
-                    if(error){
-                        return res.send({error});
-                    }
-                    
-                        res.send({
-                            location : location,
-                            address : req.query.address,
-                            forecast : forecastData
-                        })
-                    
-                })
-            
-        })
-    }
-})
-
-app.get('/products', (req,res)=>{
-    if (!req.query.search) {
-        return res.send({
-            error : 'You must provide a search term'
-        })
-    }
-    console.log(req.query.search);
-    res.send({
-        products : []
-    })
-})
-
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
-        title : '404',
-        name : 'Emmanuel Arkorful',
-        message :'Help article not found'
-    });d
-})
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title : '404',
-        name : 'Emmanuel Arkorful',
-        message: 'Page not found.'
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
     });
-})
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
-app.listen(port,()=>{
-    console.log('Server is up on port '+ port);
-})
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Emmanuel Arkorful',
+    message: 'Help article not found',
+  });
+  d;
+});
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Emmanuel Arkorful',
+    message: 'Page not found.',
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
+});
